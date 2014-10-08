5fe165c7a0ba9ed5668f660eaf230397e0e51a0b
Christiaan Westerbeek <chris@devotis.nl>
2014-10-08 22:23:32 +0200

    Converts lines and tabs to 2-dimensional array

--- resources/js/controllers.js
+++ resources/js/controllers.js
@@ -4,11 +4,27 @@
 /* Controllers */
 angular.module('myApp.controllers', [])
   .controller('MainCtrl', ['$scope', function($scope) {
-    console.log('$scope.source', $scope.source);
+    $scope.transposed = false;
+
+    function tab2html(s) {
+      if (!s) return;
+      
+      var a = s.split(/\n\r?/);
+      
+      if (a.length === 0) return;
+      
+      a = _.map(a, function(item) { return item.split(/\t/); });
+      return a;
+    }
+
+    //options: trim all values, transpose, remove trailing lines
 
     $scope.transform = function() {
+      var html = tab2html($scope.source);
+      console.log(html);
+
       $scope.target = {
-        value: $scope.source
+        value: html
       };
     };
 
